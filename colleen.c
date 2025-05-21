6aa61b143610eff3b170c49b124239d51ced6d62
Serena Gandum--Diagne <sgandum-@f2r3s13.clusters.42paris.fr>
2025-05-21 19:51:37 +0200

    colleen done

---
+++ colleen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*To use printf*/
+
+void    print_source_code()
+{
+    //String who contains source code
+    char *code = "#include <stdio.h>%c/*To use printf*/%c%cvoid    print_source_code()%c{%c    //String who contains source code%c    char *code = %c%s%c;%c    printf(a, 10, 10, 10, 10, 10, 10, 34, a, 34, 10, 10, 10, 10, 10, 10, 10, 10);%c}%c%cint main()%c{%c    print_source_code();%c    return (0);%c}";
+    printf(code, 10, 10, 10, 10, 10, 10, 34, code, 34, 10, 10, 10, 10, 10, 10, 10, 10);
+}
+
+int main()
+{
+    print_source_code();
+    return (0);
+}
