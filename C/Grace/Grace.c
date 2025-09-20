b26a6d02f94f3a1d73129d8ed76043d62778a7fe
Scode0 <gandumdiagne.serena@gmail.com>
2025-09-20 12:37:40 +0200

    C Grace & Sully sécurisés

--- C/Grace/Grace.c
+++ C/Grace/Grace.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
-#define FT(x) int main(void) {FILE *fd = fopen("Grace_kid.c", "w"); char *code = x; fprintf(fd, code, DOUBLE_QUOTES, NEW_LINE, code); fclose(fd); return(0);}
+#include <errno.h>
+#define FT(x) int main(void) {FILE *fd = fopen("Grace_kid.c", "w"); if(fd == NULL){perror("fopen error"); return(1);} char *code = x; fprintf(fd, code, DOUBLE_QUOTES, NEW_LINE, code); fclose(fd); return(0);}
 #define DOUBLE_QUOTES 34
 #define NEW_LINE 10
 
 /*
 	Call to the macro that contains the entire program
 */
-FT("#include <stdio.h>%2$c#define FT(x) int main(void) {FILE *fd = fopen(%1$cGrace_kid.c%1$c, %1$cw%1$c); char *code = x; fprintf(fd, code, DOUBLE_QUOTES, NEW_LINE, code); fclose(fd); return(0);}%2$c#define DOUBLE_QUOTES 34%2$c#define NEW_LINE 10%2$c%2$c/*%2$c	Call to the macro that contains the entire program%2$c*/%2$cFT(%1$c%3$s%1$c)")
+FT("#include <stdio.h>%2$c#include <errno.h>%2$c#define FT(x) int main(void) {FILE *fd = fopen(%1$cGrace_kid.c%1$c, %1$cw%1$c); if(fd == NULL){perror(%1$cfopen error%1$c); return(1);} char *code = x; fprintf(fd, code, DOUBLE_QUOTES, NEW_LINE, code); fclose(fd); return(0);}%2$c#define DOUBLE_QUOTES 34%2$c#define NEW_LINE 10%2$c%2$c/*%2$c	Call to the macro that contains the entire program%2$c*/%2$cFT(%1$c%3$s%1$c)")
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#define TAB 9
 #define DOUBLE_QUOTE 34
 #define NEW_LINE 10
 
@@ -17,9 +17,14 @@ int main(void)
 		snprintf(filename, sizeof(filename), "Sully_%d.c", i);
 		snprintf(cmd, sizeof(cmd), "clang -Wall -Wextra -Werror Sully_%d.c -o Sully_%d && ./Sully_%d", i, i, i);
 		FILE *fd = fopen(filename, "w");
-		char *code = "#include <stdio.h>%2$c#include <stdlib.h>%2$c#include <string.h>%2$c#define TAB 9%2$c#define DOUBLE_QUOTE 34%2$c#define NEW_LINE 10%2$c%2$cint main(void)%2$c{%2$c%1$cint i = %4$d;%2$c%1$cif(i > 0)%2$c%1$c{%2$c%1$c%1$cchar filename[50];%2$c%1$c%1$cchar cmd[100];%2$c%1$c%1$cif(strstr(__FILE__, %3$cSully.c%3$c) == NULL)%2$c%1$c%1$c%1$ci--;%2$c%1$c%1$csnprintf(filename, sizeof(filename), %3$cSully_%%d.c%3$c, i);%2$c%1$c%1$csnprintf(cmd, sizeof(cmd), %3$cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d && ./Sully_%%d%3$c, i, i, i);%2$c%1$c%1$cFILE *fd = fopen(filename, %3$cw%3$c);%2$c%1$c%1$cchar *code = %3$c%5$s%3$c;%2$c%2$c%1$c%1$cfprintf(fd, code, TAB, NEW_LINE, DOUBLE_QUOTE, i, code);%2$c%1$c%1$cfclose(fd);%2$c%1$c%1$csystem(cmd);%2$c%1$c}%2$c%2$c%1$creturn(0);%2$c}";
+		if(fd == NULL)
+		{
+			perror("fopen error");
+			return(1);
+		}
+		char *code = "#include <stdio.h>%1$c#include <errno.h>%1$c#include <stdlib.h>%1$c#include <string.h>%1$c#define DOUBLE_QUOTE 34%1$c#define NEW_LINE 10%1$c%1$cint main(void)%1$c{%1$c	int i = %3$d;%1$c	if(i > 0)%1$c	{%1$c		char filename[50];%1$c		char cmd[100];%1$c		if(strstr(__FILE__, %2$cSully.c%2$c) == NULL)%1$c			i--;%1$c		snprintf(filename, sizeof(filename), %2$cSully_%%d.c%2$c, i);%1$c		snprintf(cmd, sizeof(cmd), %2$cclang -Wall -Wextra -Werror Sully_%%d.c -o Sully_%%d && ./Sully_%%d%2$c, i, i, i);%1$c		FILE *fd = fopen(filename, %2$cw%2$c);%1$c		if(fd == NULL)%1$c		{%1$c			perror(%2$cfopen error%2$c);%1$c			return(1);%1$c		}%1$c		char *code = %2$c%4$s%2$c;%1$c%1$c		fprintf(fd, code, NEW_LINE, DOUBLE_QUOTE, i, code);%1$c		fclose(fd);%1$c		system(cmd);%1$c	}%1$c%1$c	return(0);%1$c}";
 
-		fprintf(fd, code, TAB, NEW_LINE, DOUBLE_QUOTE, i, code);
+		fprintf(fd, code, NEW_LINE, DOUBLE_QUOTE, i, code);
 		fclose(fd);
 		system(cmd);
 	}
